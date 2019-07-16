31ae821ec9b6a15cfe24b6d90fef45733f9305b4
rsudam <raghu.sairam@gmail.com>
2019-07-16 22:04:19 +0530

    working on stack view

--- tinder/ViewController.swift
+++ tinder/ViewController.swift
@@ -12,7 +12,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let subViews = [UIColor.gray,.darkGray,.black].map { (color) -> UIView in
+            let v = UIView()
+            v.backgroundColor = color
+            return v
+        }
+        
+        let topStackView = UIStackView(arrangedSubviews: subViews)
+        topStackView.distribution = .fillEqually
+        topStackView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        
+        let yellowView = UIView()
+        yellowView.backgroundColor = .yellow
+        yellowView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+        
+        let stackView = UIStackView(arrangedSubviews: [topStackView,blueView,yellowView])
+        //stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        
+        
+        view.addSubview(stackView)
+
+        stackView.fillSuperview()
+        
     }
 
 
